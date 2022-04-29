95464ddd00625d24e51e53a8db1a381a88956d72
lyman60740 <93662090+lyman60740@users.noreply.github.com>
2022-04-29 19:46:34 +0200

    Update java.js

--- java.js
+++ java.js
@@ -2,8 +2,9 @@ var l = document.querySelector('.progress-value')
 
 l.addEventListener('animationend', function(){
     var p = document.querySelector('.logo')
-    var c = document.querySelector('.charge')
-    p.innerHTML = '<img src="assets/JeremBG.jpeg" alt=""><p style="font-size:1em;margin-top: 51px; border-radius:15px;">Bon y\'a pas encore de site alors voilà une photo de BG pour vous faire patienter</p>'
-    c.innerHTML = '<p>Terminé !</p>'
+    var c = document.querySelector('.progress')
+    p.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/2zj790ncE5A?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><iframe width="0" height="0" src="https://www.youtube.com/embed/-se0o7rhGko?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><p style="font-size:1.5em;margin-top: 51px;font-family: "Poppins, sans-serif;">Bon y\'a pas encore site alors voilà une photo de BG pour vous faire patienter</p>'
+    c.innerHTML = '<p>LEZGO !</p>'
     console.log(p)
 } )
+
