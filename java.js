b2a0973953c79a84249b6bf6984d72a6a26dc96b
lyman60740 <93662090+lyman60740@users.noreply.github.com>
2022-04-29 16:13:20 +0200

    Update java.js

--- java.js
+++ java.js
@@ -2,6 +2,6 @@ var l = document.querySelector('.progress-value')
 
 l.addEventListener('animationend', function(){
     var p = document.querySelector('.logo')
-    p.innerHTML = '<img src="assets/JeremBG.jpeg" alt="">'
+    p.innerHTML = '<img style="border-radius: 5px"; src="assets/JeremBG.jpeg" alt="">'
     console.log(p)
 } )
