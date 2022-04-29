aab7f8816c3212e3d4003e8d6b728a793ba48c44
lyman60740 <93662090+lyman60740@users.noreply.github.com>
2022-04-29 17:00:08 +0200

    Update java.js

--- java.js
+++ java.js
@@ -3,7 +3,7 @@ var l = document.querySelector('.progress-value')
 l.addEventListener('animationend', function(){
     var p = document.querySelector('.logo')
     var c = document.querySelector('.charge')
-    p.innerHTML = '<img src="/assets/JeremBG.jpeg" alt=""><p style="font-size:1em;margin-top: 51px;">Bon y\'a pas encore site alors voilà une photo de BG pour vous faire patienter</p>'
+    p.innerHTML = '<img src="assets/JeremBG.jpeg" alt=""><p style="font-size:1em;margin-top: 51px;">Bon y\'a pas encore site alors voilà une photo de BG pour vous faire patienter</p>'
     c.innerHTML = '<p>Terminé !</p>'
     console.log(p)
 } )
