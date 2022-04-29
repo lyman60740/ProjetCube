7dab58645952e6e47dfcdf4a4f4e804e8b2ba478
lyman60740 <93662090+lyman60740@users.noreply.github.com>
2022-04-29 20:40:42 +0200

    Update java.js

--- java.js
+++ java.js
@@ -2,9 +2,9 @@ var l = document.querySelector('.progress-value')
 
 l.addEventListener('animationend', function(){
     var p = document.querySelector('.logo')
-    var c = document.querySelector('.progress')
-    p.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/2zj790ncE5A?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><iframe width="0" height="0" src="https://www.youtube.com/embed/-se0o7rhGko?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><p style="font-size:1.5em;margin-top: 51px;font-family: "Poppins, sans-serif;">MAIS QUI FAIT LA FÊTE ICI ?</p>'
-    c.innerHTML = '<p>LEZGO !</p>'
-    console.log(p)
+    var c = document.querySelector('.charge')
+    p.innerHTML = '<iframe width="1" height="1" src="https://www.youtube.com/embed/OdnK_sitSyw?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><img style="border-radius:10px;" src="assets/JeremMerguez.jpg"></img><p style="font-size:1.5em;margin-top: 51px;font-family: "Poppins, sans-serif;">MAIS QUI FAIT LA FÊTE ICI ?</p>'
+    c.innerHTML = '<p>Terminé !</p>'
+
 } )
 
