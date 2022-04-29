63e5db1468f31868ddb087ba750a531c5a7c48cb
lyman60740 <93662090+lyman60740@users.noreply.github.com>
2022-04-29 16:04:47 +0200

    Create java.js

---
+++ java.js
@@ -0,0 +1,7 @@
+var l = document.querySelector('.progress-value')
+
+l.addEventListener('animationend', function(){
+    var p = document.querySelector('.logo')
+    p.innerHTML = '<img src="/assets/JeremBG.jpeg" alt="">'
+    console.log(p)
+} )
